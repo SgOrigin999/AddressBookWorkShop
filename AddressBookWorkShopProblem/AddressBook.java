cddc2fca055bc3866b1761f81f60dd2e983acafb
SgOrigin999 <you@example.com>
2021-06-22 09:41:41 +0530

    Welcome to AddressBook Problem Solving

---
+++ AddressBookWorkShopProblem/AddressBook.java
@@ -0,0 +1,14 @@
+package AddressBookWorkShopProblem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class AddressBook {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome To AddressBook WorkShop Problem::");
+	}
+
+}
